5d0ca7797eec4a3825980e773fcc0a785cd249e3
Sofy24 <sofia.tosi4@studio.unibo.it>
2024-04-07 23:17:06 +0200

    feat: account-page

--- src/app/features/account/account-page/account-page.component.ts
+++ src/app/features/account/account-page/account-page.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-account-page',
   templateUrl: './account-page.component.html',
-  styleUrls: ['./account-page.component.css']
+  styleUrls: ['./account-page.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AccountPageComponent implements OnInit {
 
