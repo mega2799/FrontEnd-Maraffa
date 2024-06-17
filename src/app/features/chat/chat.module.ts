b7a6c1ed9f55c5ec6f59605bbcbfc5009cc8a6da
Sofy24 <sofia.tosi4@studio.unibo.it>
2024-06-17 11:14:43 +0200

    fix: chatmodule

--- src/app/features/chat/chat.module.ts
+++ src/app/features/chat/chat.module.ts
@@ -1,15 +1,11 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { NbChatModule, NbLayoutModule, NbThemeModule } from "@nebular/theme";
 import { ChatComponent } from "./chat.component";
 
 @NgModule({
   declarations: [ChatComponent],
   imports: [
-    NbChatModule,
     CommonModule,
-    NbLayoutModule,
-    NbThemeModule.forRoot({ name: "default" }),
   ],
   exports: [ChatComponent],
 })
