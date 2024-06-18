020360db9a9c6266bd8b6a9113268dbc48d100a6
Sofy24 <sofia.tosi4@studio.unibo.it>
2024-06-19 00:03:20 +0200

    fix: default game mode

--- src/app/features/dashboard/dashboard-home/dashboard-home.component.ts
+++ src/app/features/dashboard/dashboard-home/dashboard-home.component.ts
@@ -62,8 +62,10 @@ export class DashboardHomeComponent implements OnInit, OnDestroy {
   }
 
   sendCreate() {
+    // const mode =
+    //   this.formMode.value.mode === "classico" ? "CLASSIC" : "ELEVEN2ZERO";
     const mode =
-      this.formMode.value.mode === "classico" ? "CLASSIC" : "ELEVEN2ZERO";
+      this.formMode.value.mode === "eleven2zero" ? "ELEVEN2ZERO" : "CLASSIC";
     const numberOfPlayers = parseInt(
       this.formMode.value.numberOfPlayers as string
     );
