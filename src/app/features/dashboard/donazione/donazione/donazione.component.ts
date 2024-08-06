b6ae47dabe576708599ccfef6ba3a7a57171257f
Matteo Santoro <matteosantoro2799@gmail.com>
2024-08-06 20:49:22 +0200

    feat: user/game list

---
+++ src/app/features/dashboard/donazione/donazione/donazione.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-donazione',
+  templateUrl: './donazione.component.html',
+  styleUrls: ['./donazione.component.css']
+})
+export class DonazioneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    return;
+  }
+
+}
